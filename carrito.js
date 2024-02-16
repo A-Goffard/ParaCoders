ad3fe6d2a6016877b5a3ab77cd86d24ff3a5430a
johanrodriguez24 <srodriguez5860@gmail.com>
2024-02-16 09:36:54 +0100

    adiciones carrito

--- carrito.js
+++ carrito.js
@@ -1,14 +1,20 @@
 const seleccionar_todo = document.querySelector (".boton_check");
 const botones = document.querySelectorAll (".boton_seleccion_carrito");
 
+
+//---------------------------------------------------------------------
+
 seleccionar_todo.addEventListener("change", (event) => {
+    botones.forEach((element) => {
+        element.checked = false;
+    })
     
     if(event.target.checked){
+        botones.forEach((element) => {
+            element.checked = true;
+        })
 
     }
-})
 
-botones.forEach(element => {
-    console.log (element)
-    
+
 });
