4e196441224e514d9db745e7c288b955e36754ae
johanrodriguez24 <srodriguez5860@gmail.com>
2024-02-16 14:28:18 +0100

    cambios carrito viernes 2:28pm

--- carrito.js
+++ carrito.js
@@ -19,3 +19,50 @@ seleccionar_todo.addEventListener("change", (event) => {
 
 });
 
+// ---------Sumar y restar productos en los botones-----------
+
+let botones_sumar= document.querySelectorAll(".btn_cm");
+let botones_restar= document.querySelectorAll(".btn_com_rest")
+
+
+botones_sumar.forEach (event=>{
+        event.addEventListener ('click', function () {
+            alert(Hola);
+        })
+})
+
+
+
+
+
+
+/* let numero_inicial
+let contador=0; */
+
+/*document.querySelectorAll[0](".btn_cm").addEventListener("click", function(){
+
+    contador++;
+    alert(contador);
+    document.querySelectorAll(".num_compra").innerHTML=contador
+
+}); */
+/* document.getElementById("primero").addEventListener("click",function(){
+    contador++;
+    alert(contador);
+    //document.querySelectorAll(".num_compra").innerHTML=contador
+
+});
+document.querySelectorAll[0](".btn_cm_rest").addEventListener("click", restarUno)
+ */
+
+
+//function sumarUno(){
+  
+
+//}
+
+/* function restarUno(){
+    contador= contador -1;
+    document.querySelectorAll(".num_compra").innerHTML=contador
+
+} */
